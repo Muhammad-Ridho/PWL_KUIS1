b5d44122b166cd3e1902e7808fbbb80b49fff7d5
= <ramadhanjenius1@gmail.com>
2021-03-10 22:01:38 +0700

    pwlkuis1

--- database/migrations/2021_03_10_145623_barang.php
+++ database/migrations/2021_03_10_145623_barang.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class Barang extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,16 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        //
+        Schema::create('barang', function (Blueprint $table) {
             $table->id();
             $table->string('title', 100)->index();
             $table->string('slug', 100)->index();
+            $table->string('image', 255)->nullable();
             $table->text('content');
             $table->boolean('draft')->default(false);
             $table->timestamps();
             });
-        
-        
-           
     }
 
     /**
@@ -33,7 +32,7 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
-       
+        //
+        Schema::dropIfExists('barang');
     }
 }
