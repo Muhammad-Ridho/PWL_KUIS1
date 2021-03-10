341b33a476fd1590abed669922b93148f35756f1
= <ramadhanjenius1@gmail.com>
2021-03-10 21:12:30 +0700

    pwlkuis1

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,6 @@ use App\Http\Controllers\HomeController;
     Route::get('/features', [HomeController::class, 'features']);
     Route::get('/contact', [HomeController::class, 'contact']);
     Route::get('/barang', [HomeController::class, 'barang']);
+    Route::get('/pelanggan', [HomeController::class, 'pelanggan']);
+    Route::get('/pegawai', [HomeController::class, 'pegawai']);
+    Route::get('/supplier', [HomeController::class, 'supplier']);
