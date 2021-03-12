d60cef9fe4ff04fff6b8aa877d56b615934c6259
= <ramadhanjenius1@gmail.com>
2021-03-12 10:12:02 +0700

    pwlkuis1

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\BarangController;
+use App\Http\Controllers\PelangganController;
+
 
 /*
 |--------------------------------------------------------------------------
